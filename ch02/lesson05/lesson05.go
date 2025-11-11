6680d954cbe102bf5eae1858396fbf07e67dd4b6
Joshua Liew <liqshann@gmail.com>
2025-11-11 20:45:21 +0900

    ch2, l5: conditionals practice

---
+++ ch02/lesson05/lesson05.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var insufficientFundMessage string = "Purchase failed. Insufficient funds."
+	var purchaseSuccessMessage string = "Purchase successful."
+	var accountBalance float64 = 100.0
+	var bulkMessageCost float64 = 75.0
+	var isPremiumUser bool = true
+	var discountRate float64 = 0.10
+	var finalCost float64
+
+	finalCost += bulkMessageCost
+	if isPremiumUser {
+		finalCost -= (finalCost * discountRate)
+	}
+	if finalCost <= accountBalance {
+		accountBalance -= finalCost
+		fmt.Println(purchaseSuccessMessage)
+	} else {
+		fmt.Println(insufficientFundMessage)
+	}
+
+	fmt.Println("Account balance:", accountBalance)
+}
