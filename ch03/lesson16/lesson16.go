6dd440390a92ff8bf7e7dd181e8804af878ff2d9
Joshua Liew <liqshann@gmail.com>
2025-11-12 22:05:22 +0900

    ch3, l16: anonymous functions

---
+++ ch03/lesson16/lesson16.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func printReports(intro, body, outro string) {
+	printCostReport(func(s string) int {
+		return len(s) * 2
+	}, intro)
+	printCostReport(func(s string) int {
+		return len(s) * 3
+	}, body)
+	printCostReport(func(s string) int {
+		return len(s) * 4
+	}, outro)
+}
+
+func main() {
+	printReports(
+		"Welcome to the Hotel California",
+		"Such a lovely place",
+		"Plenty of room at the Hotel California",
+	)
+}
+
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
+	fmt.Println()
+}
