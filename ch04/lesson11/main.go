a5f639162edaceb6187dc8152bad18e019504f34
Joshua Liew <liqshann@gmail.com>
2025-11-13 21:48:46 +0900

    ch4, l11: more struct practice

---
+++ ch04/lesson11/main.go
@@ -0,0 +1,29 @@
+package main
+
+func (u User) SendMessage(message string, messageLength int) (string, bool) {
+	if messageLength > u.MessageCharLimit {
+		return "", false
+	}
+	return message, true
+}
+
+type User struct {
+	Name string
+	Membership
+}
+
+type Membership struct {
+	Type string
+	MessageCharLimit int
+}
+
+func newUser(name string, membershipType string) User {
+	membership := Membership{Type: membershipType}
+	if membershipType == "premium" {
+		membership.MessageCharLimit = 1000
+	} else {
+		membership.Type = "standard"
+		membership.MessageCharLimit = 100
+	}
+	return User{Name: name, Membership: membership}
+}
