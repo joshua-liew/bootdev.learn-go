82d8a0abbc9ba954c55c4c003589c3ecf005ac2b
Joshua Liew <liqshann@gmail.com>
2025-11-17 17:40:49 +0900

    ch8, l19: slices prac #3

---
+++ ch08/lesson19/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strings"
+)
+
+type sms struct {
+	id 		string
+	content	string
+	tags	[]string
+}
+
+func tagMessages(messages []sms, tagger func(sms) []string) []sms {
+	for i := 0; i < len(messages); i++ {
+		messages[i].tags = tagger(messages[i])
+		if messages[i].tags == nil {
+			return nil
+		}
+	}
+	return messages
+}
+
+func tagger(msg sms) []string {
+	tags := []string{}
+	if strings.Contains(strings.ToLower(msg.content), "urgent") {
+		tags = append(tags, "Urgent")
+	}
+	if strings.Contains(strings.ToLower(msg.content), "sale") {
+		tags = append(tags, "Promo")
+	}
+	return tags
+}
